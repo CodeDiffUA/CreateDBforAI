f5d6f07e81fcd81c796396d51a797c0a61217582
Danylo Romanchak <dan.romanch@gmail.com>
2024-03-05 12:02:22 +0200

    add function redicting to link

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as wait
 
 
 def parse_links():
@@ -18,4 +22,19 @@ def parse_links():
     return links
 
 
-parse_links()
+def go_to_links(links):
+    browser = webdriver.Firefox()
+    for i in links:
+        print(f"https://zno.osvita.ua{i}")
+        browser.get(f"https://zno.osvita.ua{i}")
+        content = browser.page_source
+        soup = BeautifulSoup(content, 'html.parser')
+        spans = soup.find('div', {'class': 'tasks-numbers'}).find_all('span')
+
+        wait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="tasks-numbers"]/span[{spans[-1].text}]'))).click()
+        wait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="q_form_39"]/div[7]/div[2]/span'))).click()
+        for i in range (1, len(spans) + 1):
+            wait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="tasks-numbers"]/span[{i}]'))).click()
+
+
+go_to_links(parse_links())
