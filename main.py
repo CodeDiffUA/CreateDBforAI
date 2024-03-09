c4493497d21f2d06b815758efe89b19a08f5b524
Danylo Romanchak <dan.romanch@gmail.com>
2024-03-10 01:38:57 +0200

    Fixed bug with questions and answers

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.firefox.options import Options
 
+#todo make headless
+
+options = Options()
+options.headless = True
 load_dotenv()
 connection_string = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(connection_string)
 cur = conn.cursor()
 
+
 def parse_links():
     links = []
-    browser = webdriver.Firefox()
+    browser = webdriver.Firefox(options=options)
     browser.get('https://zno.osvita.ua/ukrainian/tema.html')
     content = browser.page_source
     soup = BeautifulSoup(content, 'html.parser')
@@ -25,11 +29,12 @@ def parse_links():
         for i in r:
             links.append(i[6:-2])
     browser.close()
+    links.pop()
     return links
 
 
 def parse_data(links):
-    browser = webdriver.Firefox()
+    browser = webdriver.Firefox(options=options)
     for i in links:
         browser.get(f"https://zno.osvita.ua{i}")
         content = browser.page_source
@@ -38,30 +43,56 @@ def parse_data(links):
             spans = soup.find('div', {'class': 'tasks-numbers'}).find_all('span')
         except:
             continue
-        wait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="tasks-numbers"]/span[{spans[-1].text}]'))).click()
+        browser.find_element(By.XPATH, f'//*[@id="tasks-numbers"]/span[{spans[-1].text}]').click()
         try:
-            wait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, f'//*[@id="q_form_{spans[-1].text}"]/div[7]/div[2]/span'))).click()
+            browser.find_element(By.XPATH, f'//*[@id="q_form_{spans[-1].text}"]/div[7]/div[2]/span').click()
         except:
-            continue
-        page_source = browser.page_source
-        soup = BeautifulSoup(page_source, 'html.parser')
+            browser.find_element(By.XPATH, f'//*[@id="q_form_{spans[-1].text}"]/div[8]/div[2]/span').click()
+        content = browser.page_source
+        soup = BeautifulSoup(content, 'html.parser')
         tasks = soup.findAll('div', {'class': 'task-card'})
         subject = soup.findAll('span', {'class': 'row'})[0].text
         chapter = soup.findAll('span', {'class': 'row'})[1].text
         theme = soup.findAll('span', {'class': 'row'})[2].text
         for j in range(1, len(tasks)):
-            question = soup.find('div', {'id': f'q{j}'}).find('div', {'class': 'question'}).text
-            # todo bug cant understand which letter bald which not
+            question = str(soup.find('div', {'id': f'q{j}'}).find('div', {'class': 'question'}))
+            question = format_html(question)
             answers = soup.find('div', {'id': f'q{j}'}).findAll('div', {'class': 'answer'})
             answer_list = []
-            for k in answers:
-                answer = k.text[1:]
+            for answer in answers:
+                answer = format_html(str(answer))
                 answer_list.append(answer)
             create_dict(subject, question, chapter, theme)
-            # correct_answer = (soup.find('div', {'id': f'q{j}'}).find('table', {'class': 'select-answers-variants'})).findAll('span')
+            #correct_answer = str((soup.find('div', {'id': f'q{j}'}).find('table', {'class': 'select-answers-variants'})))
+            #print(correct_answer)
+            #for ii in correct_answer:
+            #p = re.findall("class=\"marker\"|class=\"marker ok\"", correct_answer)
             # #todo idk how to change link after pressing button
-            # for ii in correct_answer:
-            #     x = re.findall("class=\"marker\"|class=\"marker ok\"", ii)
+            # #todo add vidpovidnosti to answers
+            # #todo explanation
+
+
+def format_html(html_string):
+    html_string = html_string.replace("<strong>", "**").replace("</strong>", "**")
+    html_string = html_string.replace("<em>", "*").replace("</em>", "*")
+    html_string = html_string.replace("<u>", "__").replace("</u>", "__")
+    html_string = html_string.replace("<b>", "**").replace("</b>", "**")
+    html_string = html_string.replace("<i>", "*").replace("</i>", "*")
+    html_string = html_string.replace("<div class=\"question\">", "").replace("/div", "")
+    html_string = html_string.replace("<p>", "").replace("</p>", "")
+    html_string = html_string.replace("<br/>", "")
+    html_string = html_string.replace("<>", "")
+    q = re.findall("<style>\n.*\n</style>", html_string)
+    for b in q:
+        html_string = html_string.replace(b, "")
+    y = re.findall("<style>.*</style>", html_string)
+    for m in y:
+        html_string = html_string.replace(m, "")
+    x = re.findall("<.*>", html_string)
+    for c in x:
+        html_string = html_string.replace(c, "")
+    return html_string
+
 
 
 def create_dict(subject, question, chapter, theme):
@@ -80,10 +111,11 @@ def connect_to_db(data):
 
 def main():
     cur.execute("DROP TABLE IF EXISTS ukrmova")
-    cur.execute("CREATE TABLE ukrmova (subject VARCHAR(1000), question VARCHAR(1000), chapter VARCHAR(1000), theme VARCHAR(1000))")
+    cur.execute("CREATE TABLE ukrmova (subject VARCHAR(9000), question VARCHAR(9000), chapter VARCHAR(9000), theme VARCHAR(9000))")
     conn.commit()
     parse_data(parse_links())
     cur.close()
     conn.close()
+
 
 main()
